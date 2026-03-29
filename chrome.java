452fccbe776c0fb4536579e52b81a36109f669fc
Anitha43557 <anithaashanmugam90@gmail.com>
2026-03-29 18:52:35 +0530

    week1day1assignment

---
+++ chrome.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class chrome {
+public static void main(String[] args) {
+    float broswerVersion= 91.0f;
+    String browserName= "chrome";
+    boolean isVisible= false;
+    int releaseYear= 2008;
+    char broswerLogo= 'c';
+    System.out.println("broswerVersion = "+broswerVersion+"\n"+
+        "broswerName = "+browserName+"\n"+
+        "isVisible = "+isVisible+"\n"+
+        "releaseYear = "+releaseYear+"\n"+
+        "broswerLogo = "+broswerLogo);
+
+}
+}
